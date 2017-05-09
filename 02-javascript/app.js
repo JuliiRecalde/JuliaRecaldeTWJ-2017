1eb0b242b79e27cfd2e6c5fdfa9499b28ac52709
mordekai23 <danilo48_its@yahoo.com>
2017-05-09 16:31:28 -0500

    Template Strings

--- 02-javascript/app.js
+++ 02-javascript/app.js
@@ -6,10 +6,26 @@
 //comentarios una sola linea
 
 console.log('hello Kitty');
-var nombre = {
+var julia = {
     nombre:"julia",
     apelido:"recalde",
+    fechaNacimiento: new Date(),
     imprimir:function(){
-        
+        console.log(this.nombre, this.apelido);      //console.log(julia.nombre, julia.apelido)
+
+    },
+    ano:function () {
+        return this.fechaNacimiento.getFullYear();
+
+    },
+    diferenciaEdad: function (edadAComparar) {
+        //Primera forma
+        return Math.abs(edadAComparar-this.fechaNacimiento.getFullYear())
+
     }
-}
+};
+
+console.log(julia.ano());
+console.log(julia.diferenciaEdad(1993));
+julia.imprimir();
+
