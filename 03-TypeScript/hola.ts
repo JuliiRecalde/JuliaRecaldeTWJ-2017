9094263ed92523f5c12222e7bfe3aca901cca998
USRDEL <USRDEL@USR13.local>
2017-05-16 15:35:16 -0500

    tipos de datos

--- 03-TypeScript/hola.ts
+++ 03-TypeScript/hola.ts
@@ -1,4 +1,25 @@
 /**
  * Created by USRDEL on 16/5/17.
  */
+
+
+
+let holaMundo = 'Hola mundoi cruel';
+
+
+//boolean
+let soltero:boolean = true;
+
+//enteros
+let novias=12;
+
+//string
+let frase= "You're done";
+
+let otraFrase= `El numero de novias en ${novias +1}`;
+
+//arrays
+let notas :number []=[1, 2, 34]; //[]
+let jaladas:Array<number>=[1, 2, 34] //[]
+
 console.log ("Hola mundo");
