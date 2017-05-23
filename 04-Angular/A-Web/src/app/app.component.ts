01588f8be3d1473532a27c11e28b531b1ef3ceb5
JuliiRecalde <USRDEL@USR13.local>
2017-05-23 16:37:51 -0500

    clase del 23 de mayo

--- 04-Angular/A-Web/src/app/app.component.ts
+++ 04-Angular/A-Web/src/app/app.component.ts
@@ -7,21 +7,35 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   usuario:UsuarioInt ={
-    nombre: "Julia",
+    nombre: "",
     apellido: "Recalde"
-  }
+
+  };
+  parrafo:string = "";
+  //background:string="blue";
 
   url:string="https://angular.io/";
   src:string="https://g00glen00b.be/wp-content/uploads/2016/11/angular.png";
 
+
   constructor(){
-    console.log("constructor");
+    /*console.log("constructor");
     this.holaMundo();
     console.log(
       this.saludar(this.usuario.nombre,
         this.usuario.apellido
-      ));
+      ));*/
+
+
+    this.usuario.nombre = "Patricia";
+    this.parrafo = "Mi mama me mima";
+
+    setTimeout(()=>{
+      this.usuario.nombre = "Julia"
+
+    },3000);
   }
+
   //funciones (publica y void)
   holaMundo(){
     console.log("Hola mundo");
--- 04-Angular/A-Web/src/app/app.module.ts
+++ 04-Angular/A-Web/src/app/app.module.ts
@@ -4,10 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { EstiloComponent } from './estilo/estilo.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EstiloComponent
   ],
   imports: [
     BrowserModule,
