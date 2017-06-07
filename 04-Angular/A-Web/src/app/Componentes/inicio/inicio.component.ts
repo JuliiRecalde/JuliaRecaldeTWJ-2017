4cfeae673b45eb77e846a9176cfc238e8089cdac
JuliiRecalde <USRDEL@USR13.local>
2017-06-07 14:55:46 -0500

    interfaz inicio

--- 04-Angular/A-Web/src/app/Componentes/inicio/inicio.component.ts
+++ 04-Angular/A-Web/src/app/Componentes/inicio/inicio.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {Http} from "@angular/http";
 import 'rxjs/add/operator/map';
 import {PlanetasStarWars} from "../../Interfaces/PlanetaStarWars";
+import {UsuarioClass} from "../../Clases/UsuarioClase";
 
 @Component({
   selector: 'app-inicio',
@@ -9,13 +10,31 @@ import {PlanetasStarWars} from "../../Interfaces/PlanetaStarWars";
   styleUrls: ['./inicio.component.css']
 })
 export class InicioComponent implements OnInit {
+
+  usuario:UsuarioClass[]=[];
+
+  nuevoUsuario:UsuarioClass= new UsuarioClass();
+
   planetas: PlanetasStarWars[]=[];
   //planetas2:Array<PlanetasStarWars>=[];
+
+
 
   constructor(private _http: Http) {
   }
 
   ngOnInit() {
+    this._http.get("http://localhost:1337/Usuario")
+      .subscribe(
+        respuesta=>{
+          let rjson:UsuarioClass[] = respuesta.json();
+          this.usuario=rjson;
+          console.log("Usuarios ", this.usuario);
+        },
+        error=>{
+          console.log("Error ", error)
+        }
+      )
   }
 
   cargarPlanetas() {
@@ -46,6 +65,27 @@ export class InicioComponent implements OnInit {
       )
   }
 
+  crearUsuario(){
+    console.log("Entro a crear usuario");
+    /*
+    let usuario:UsuarioClass={
+      nombre:this.nuevoUsuario.nombre
+    }
+    */
+    this._http.post("http://localhost:1337/Usuario", this.usuario)
+      .subscribe(
+        respuesta=>{
+          let respuestaJson =respuesta.json();
+          console.log("respuestaJson: ", respuestaJson)
+        },
+        error=>{
+          console.log("error", error)
+        }
+      )
+  }
+
 
 }
+
+
 
